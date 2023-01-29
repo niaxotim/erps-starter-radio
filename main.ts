be9be22debda1d9173e6311dc55cac09022de6de
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-29 10:44:37 +0000

    Blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "A") {
+        basic.showIcon(IconNames.Snake)
+    } else if (receivedString == "B") {
+        basic.showIcon(IconNames.Ghost)
+    } else if (receivedString == "shake") {
+        basic.showIcon(IconNames.Chessboard)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("shake")
+})
+radio.setGroup(255)
